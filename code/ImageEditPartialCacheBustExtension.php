426e07e9d71a799d85989699b9fb5caa4012f173
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-21 16:45:38 +0700

    FIX: Whitespace

--- code/ImageEditPartialCacheBustExtension.php
+++ code/ImageEditPartialCacheBustExtension.php
@@ -3,9 +3,10 @@
 class ImageEditPartialCacheBustExtension extends DataExtension {
 
 	/*
-	This is intended to be added to Image as an extension.  When an image is edited  checks are made
-	against a configurable list of classes mapped to the image ID field.  If the image ID field value matches
-	the  ID of the image being refocussed, the DataObject's LastEdited field is updated.
+	This is intended to be added to Image as an extension.  When an image is edited  checks are
+	made against a configurable list of classes mapped to the image ID field.  If the image ID
+	field value matches the  ID of the image being refocussed, the DataObject's LastEdited field
+	is updated.
 	*/
 	public function onAfterWrite() {
 		$config = Config::inst();
@@ -22,14 +23,13 @@ class ImageEditPartialCacheBustExtension extends DataExtension {
 					foreach ($stages as $stage) {
 						$suffix = '_'.$stage;
 						$suffix = str_replace('_Stage', '', $suffix);
-						$sql = "UPDATE `SiteTree{$suffix}` SET LastEdited=NOW() where ID=".$objectWithImage->ID;
+						$sql = "UPDATE `SiteTree{$suffix}` SET LastEdited=NOW() where ID=".
+							$objectWithImage->ID;
 						DB::query($sql);
 					}
 				}
 			}
 		}
-
-		
 
 		if ($dataobjectclasses) {
 			// deal with SiteTree first
@@ -37,9 +37,9 @@ class ImageEditPartialCacheBustExtension extends DataExtension {
 				$instanceofclass = Injector::inst()->create($clazz);
 				$objectsWithImage = $instanceofclass::get()->filter($idfield, $this->owner->ID);
 				foreach ($objectsWithImage as $objectWithImage) {
-						$sql = "UPDATE `$clazz` SET LastEdited=NOW() where ID=".$objectWithImage->ID;
+						$sql = "UPDATE `$clazz` SET LastEdited=NOW() where ID=".
+							$objectWithImage->ID;
 						DB::query($sql);
-					
 				}
 			}
 		}
